e81c1ed54e93809a39e866f4f349446abe4ba31c
ankur-cmd1 <almadi.ankur7@gmail.com>
2025-03-03 17:32:18 -0500

    Add a placeholder main function

--- guess.py
+++ guess.py
@@ -3,6 +3,10 @@ import random
 num = random.randint(1, 10)
 guess = None
 
+def main():
+    """This is a dummmy function"""
+    pass
+
 while guess != num:
     guess = input("guess a number between 1 and 10: ")
     guess = int(guess)
